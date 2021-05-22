332f7596c1f0ffcb131d0599112cb322a77100e3
Guille Gigeroa <guillermo.gigeroa@hotmail.com>
2021-05-22 17:04:18 -0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "view/view.h"
+
+using namespace std;
+
+int main(){
+    cin.get();
+    return 0;
+}
---
+++ view/view.h
@@ -0,0 +1,11 @@
+#ifndef VIEW_H_INCLUDED
+#define VIEW_H_INCLUDED
+
+using namespace std;
+
+class view(){
+public:
+
+};
+
+#endif // VIEW_H_INCLUDED
